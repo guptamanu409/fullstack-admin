4942b4fcba518fdbaf201592cdcf189276f74e9a
Tanjiro <guptamanu409@gmail.com>
2022-12-08 00:36:11 +0530

    initial commit

---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box)({ //css properties
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+});
+
+export default FlexBetween;
+//re use this set of properties
